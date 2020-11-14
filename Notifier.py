595fc3cabcbe10fb99bf113732e4dd3bf2385d37
Fabian Geyer <fbn.geyer@gmail.com>
2020-11-14 23:24:28 +0100

    add notify_by_id function to Notifier

--- Notifier.py
+++ Notifier.py
@@ -15,4 +15,14 @@ class Notifier:
         self.bot = Bot(token=conf["bot_token"])
 
     def notify_user(self, message: str, user: User):
-        self.bot.send_message(chat_id=user.get_chat_id(), text=message, parse_mode=ParseMode.MARKDOWN)
+        self.bot.send_message(
+            chat_id=user.get_chat_id(),
+            text=message,
+            parse_mode=ParseMode.MARKDOWN)
+
+    def notify_by_chat_id(self, message: str, chat_id: int):
+        self.bot.send_message(
+            chat_id=chat_id,
+            text=message,
+            disable_web_page_preview=True
+            )
