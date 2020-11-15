4375c4f5c56f2efb0f3b9d88c2c06f301f852aee
Fabian Geyer <fbn.geyer@gmail.com>
2020-11-15 17:37:35 +0100

    rework hourly task to be called by cron

--- hourly_task.py
+++ hourly_task.py
@@ -3,17 +3,71 @@ from Notifier import Notifier
 from Training import Training
 import datetime
 import constants as c
+import time
 
-def hourly_check():
-    """[summary]
+
+def get_message(training: dict, subtraining: dict) -> str:
+    """Generates a message to be sent when the training starts
+
+    :param training: training date
+    :type training: dict
+    :param subtraining: subtraining data
+    :type subtraining: dict
+    :return: message to be sent
+    :rtype: str
     """
+    message = "Das Training startet jetzt!" +\
+            "\n\n Starte durch mit der Erwärmung:"\
+            "\n" + training["link"] +\
+            "\n\nDanach gibt's dein Training bei " + subtraining["coach"]["full_name"] + \
+            ": \n" + subtraining["link"] + \
+            "\n Viel Spaß!"
+    return message
+
+def notify_all_attendees(training: dict, notifier: Notifier):
+    """notify all attendees about their traings now.
+
+    :param training: training data
+    :type training: dict
+    :param notifier: notifier instance
+    :type notifier: Notifier
+    """
+    for sub in training["subtrainings"]:
+        message = get_message(training=training, subtraining=sub)
+        for att in sub["attendees"]:
+            notifier.notify_by_chat_id(
+                message=message,
+                chat_id=att["chat_id"]
+            )
+    return
+
+def wait_for_training(training: dict, timedelta: object, notifier: Notifier):
+    """Wait until the training starts and then send a message to all
+    attendees
+
+    :param training: training date
+    :type training: dict
+    :param timedelta: timedelta object until training start
+    :type timedelta: object
+    :param notifier: notifier instance
+    :type notifier: Notifier
+    """
+    time_to_sleep = timedelta
+    time.sleep(time_to_sleep.total_seconds())
+    notify_all_attendees(training=training, notifier=notifier)
+    
+def main():
+    notifier = Notifier()
     db = Database(c.CONFIG_FILE, debug_mode=True)
     next_training = db.next_trainings(number_of_trainings=1)[0]
     now = datetime.datetime.now()
-    timedelta = datetime.timedelta(minutes=40)
-    if (next_training["date"]-now < timedelta):
-        print("training in less than " + str(timedelta))
-    else:
-        print("next training in: " + str(next_training["date"]-now)[:-10] + "h")
-
-hourly_check()
+    time_to_training = next_training["date"] - now
+    if  time_to_training < datetime.timedelta(hours=1):
+        wait_for_training(
+            training=next_training,
+            timedelta=time_to_training,
+            notifier=notifier)
+    
+if __name__ == "__main__":
+    """ run this file every hour"""
+    main()
